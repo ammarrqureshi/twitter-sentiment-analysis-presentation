61335862a057bb299ed426f91a11c4be5511ed6e
Ammar Qureshi <mamqureshi786@gmail.com>
2023-01-19 08:46:49 +0500

    create prediction.py
    This file was created by Ammar. Contains code to use the sentiment analysis model  to predict emotion of a certain tweet text.

---
+++ prediction.py
@@ -0,0 +1,27 @@
+#predicting the test input for tweet sentiment analysis
+
+def decode_sentiment(score, include_neutral=True):
+    if include_neutral:        
+        label = NEUTRAL
+        if score <= SENTIMENT_THRESHOLDS[0]:
+            label = NEGATIVE
+        elif score >= SENTIMENT_THRESHOLDS[1]:
+            label = POSITIVE
+
+        return label
+    else:
+        return NEGATIVE if score < 0.5 else POSITIVE
+        
+def predict(text, include_neutral=True):
+    start_at = time.time()
+    # Tokenize text
+    x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=SEQUENCE_LENGTH)
+    # Predict
+    score = model.predict([x_test])[0]
+    # Decode sentiment
+    label = decode_sentiment(score, include_neutral=include_neutral)
+
+    return {"label": label, "score": float(score),
+       "elapsed_time": time.time()-start_at}  
+
+predict("I hate the rain")
